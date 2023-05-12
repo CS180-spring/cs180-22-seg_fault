5b4eb565bf1af29e82da15ddb06d5fdadcbe0a50
samed425 <77028650+samed425@users.noreply.github.com>
2023-05-12 11:10:51 -0700

    Update document.cpp

--- document.cpp
+++ document.cpp
@@ -160,7 +160,8 @@ std::vector<Document*> filter(std::vector<Document*> docs, std::string tag)
         }
     }
     return filteredDocs;
-    
+}
+
  std::vector <Document*> Ascending(std::vector<Document*> input) {
    std::vector<Document*> temp = input;
    std::sort(temp.begin(), temp.end(), [](Document* &e1, Document* &e2){ return e1->RetName()>e2->RetName(); });
